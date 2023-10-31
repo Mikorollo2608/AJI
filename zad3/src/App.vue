41af05cd779cf6274e6223b0199c0129da5dbfef
jakkozi <242438@edu.p.lodz.pl>
2023-10-31 16:13:26 +0100

    Created structure for Ex3; Started work on Search component

---
+++ zad3/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <b-container>
+      <h1>Baza filmów</h1>
+      <SearchBar/>
+    </b-container>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from "./components/SearchBar.vue";
+
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  //text-align: center;
+  color: #212529;
+  text-align: left;
+}
+</style>
---
+++ zad3/src/components/SearchBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <b-form>
+      <b-form-group label="Tytuł" label-for="inputTitle">
+        <b-form-input type="text" id=inputTitle class="form-control" placeholder="Podaj tytuł lub fragment tytułu filmu"/>
+      </b-form-group>
+      <b-form-group label="Rok produkcji od:" label-for="inputProductionFrom" label-cols-sm="4">
+          <b-form-input type="text" id=inputProductionFrom class="form-control"  placeholder="Liczba naturalna z przedziału 1900-2019"/>
+      </b-form-group>
+      <b-form-group label="Rok produkcji do:" label-for="inputProductionTo" label-cols-sm="4">
+          <b-form-input type="text" id=inputProductionTo class="form-control" placeholder="Liczba naturalna z przedziału 1900-2019"/>
+      </b-form-group>
+      <b-form-group label="Obsada" label-for="inputCast">
+        <b-form-input type="text" id="inputCast" class="form-control" placeholder="Imię i nazwisko"/>
+      </b-form-group>
+      <b-button block type="submit" variant="info">Szukaj</b-button>
+    </b-form>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar'
+}
+</script>
+
+<style scoped>
+</style>
