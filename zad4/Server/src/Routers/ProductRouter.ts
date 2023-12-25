e74450156b252320f20c665d96a9a4e40aa553c4
jakkozi <126513914+jakkozi@users.noreply.github.com>
2023-12-25 16:26:34 +0100

    Added getting product by id

--- zad4/Server/src/Routers/ProductRouter.ts
+++ zad4/Server/src/Routers/ProductRouter.ts
@@ -3,14 +3,31 @@ import {AppDataSource} from "../data-source";
 import {Product} from "../entity/Product";
 import {ReasonPhrases, StatusCodes, getReasonPhrase, getStatusCode} from 'http-status-codes';
 import {RestError} from "../errors";
-import {validationResult} from "express-validator";
+import {checkExact, param, validationResult} from "express-validator";
 import {checkSchema, createNewProductSchema} from "../Schemas/ProductSchemas";
+import {Equal} from "typeorm";
 
 const productRepository = AppDataSource.getRepository(Product);
 
 async function getAllProducts(req: Request, res: Response): Promise<void> {
     const products: Product[] = await productRepository.find()
     res.json(products);
+}
+
+async function getProductById(req: Request, res: Response): Promise<void> {
+
+    let id:number = parseInt(req.params.id);
+    if(isNaN(id)){
+        res.sendStatus(StatusCodes.NOT_FOUND);
+        return ;
+    }
+
+    const product: Product | null = await productRepository.findOneBy({id: id });
+    if(product){
+        res.json(product);
+    } else {
+        res.sendStatus(StatusCodes.NOT_FOUND)
+    }
 }
 
 async function createNewProduct(req: Request, res: Response): Promise<void> {
@@ -23,18 +40,18 @@ async function createNewProduct(req: Request, res: Response): Promise<void> {
     }
 
     let newProduct= productRepository.create(req.body);
-    console.log(newProduct);
     try {
-        await productRepository.save(newProduct)
+        newProduct = await productRepository.save(newProduct)
     } catch (error) {
         res.status(StatusCodes.BAD_REQUEST).json(RestError.getErrorMessage(error));
         return;
     }
-    res.sendStatus(StatusCodes.CREATED);
+    res.status(StatusCodes.CREATED).json(newProduct);
 }
 
 export const router = express.Router();
 router.use(express.json());
 
 router.get('/', getAllProducts);
-router.post('/',checkSchema(createNewProductSchema), createNewProduct);
+router.get('/:id', getProductById);
+router.post('/',checkExact(checkSchema(createNewProductSchema)), createNewProduct);
